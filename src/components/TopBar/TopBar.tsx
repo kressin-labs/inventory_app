9b0cd95f07821e77b4c5cc576fbac11856e929c6
Fabian Kreßin <61588952+fabiankressin@users.noreply.github.com>
2025-11-28 15:15:29 +0100

    feat: added a link that links back to my main page

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -6,18 +6,23 @@ export default function TopBar() {
 
   return (
     <div className="topbar">
+      <a href="https://fabiankressin.com" className="main-link">
+        fabiankressin.com
+      </a>
+
+      {/* Title in the Center */}
       <div className="title">Inventory App</div>
 
+      {/* User Info on the Right */}
       <div className="right">
         {user ? (
-            <>
-                <span className="username">{user.username} ({user.role})</span>
-                <button className="logout-btn" onClick={logout}>Logout</button>
-            </>
+          <>
+            <span className="username">{user.username} ({user.role})</span>
+            <button className="logout-btn" onClick={logout}>Logout</button>
+          </>
         ) : (
-            <span>Not logged in</span>
+          <span>Not logged in</span>
         )}
-
       </div>
     </div>
   );
